74239eb231340bc989ab147613a9ff957fb00618
Nitesh Jangir <37628765+iamniteshjangir@users.noreply.github.com>
2020-12-06 23:30:54 +0530

    Update settings.py

--- driver/settings.py
+++ driver/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wi@h3urutpaug4*m5bau%36%mxci#xu85bxe&um@^rci(o0--6'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
